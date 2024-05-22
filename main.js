4e384ef650dd116b852b43971f7c18cbd8b87fd8
utsavvaghela2904 <utsavvaghela2904@gmail.com>
2024-05-22 16:36:20 +0530

    add js file and button

---
+++ main.js
@@ -0,0 +1,11 @@
+function darkbtn(){
+    var a = document.body.style;
+    a.background="black";
+    a.color="white";
+}
+
+function lightbtn(){
+    var a = document.body.style;
+    a.background="white";
+    a.color="black";
+}
